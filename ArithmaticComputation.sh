46a54190a3015d6bbc898832576adc1ee926217a
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 19:28:07 +0530

    computation UC-5

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,5 +5,5 @@ read b
 read c
 num1=$(echo $a $b $c | awk '{ print $1 + $2 * $3 }' )
 num2=$(echo $a $b $c | awk '{ print $1 * $2 + $3 }' )
-num3=$(echo $a $b $c | awk '{ print $3 + $1 / $3 }' )
+num3=$(echo $a $b $c | awk '{ print $3 + $1 / $2 }' )
 # echo "$num1 and $num2 $num3"
