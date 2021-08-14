76b510f39726bcb8ad1c0380de82389e1ed709ea
Saddam Khan <54209179+sdmkhan123@users.noreply.github.com>
2021-08-14 18:22:31 +0530

    Update ArithmaticComputation.sh

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,4 +5,4 @@ read b
 read c
 num1=$(echo $a $b $c | awk '{ print $1 + $2 * $3 }' )
 num2=$(echo $a $b $c | awk '{ print $1 * $2 + $3 }' )
-echo "computation one two and three are :" $num1 $num2 $num3
+echo "computation one and two are :" $num1 $num2
