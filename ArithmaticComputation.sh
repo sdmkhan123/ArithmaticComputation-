4fa206ef2c347d6c3001d4d892e9ddadd43e589a
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-14 19:46:33 +0530

    computation UC-6

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-
+declare -A dictionary
 read a
 read b
 read c
@@ -7,4 +7,8 @@ num1=$(echo $a $b $c | awk '{ print $1 + $2 * $3 }' )
 num2=$(echo $a $b $c | awk '{ print $1 * $2 + $3 }' )
 num3=$(echo $a $b $c | awk '{ print $3 + $1 / $2 }' )
 num4=$(echo $a $b $c | awk '{ print $1 % $2 + $3 }' )
-echo "computation one two three and four are :" $num1 $num2 $num3 $num4
+dictionary[0]=$num1
+dictionary[1]=$num2
+dictionary[2]=$num3
+dictionary[4]=$num4
+echo "computation values:" ${dictionary[*]}
