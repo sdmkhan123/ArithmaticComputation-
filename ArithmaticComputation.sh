10f7690d878a2b17e0f0831387cc1956c535869d
Saddam Khan <54209179+sdmkhan123@users.noreply.github.com>
2021-08-14 18:08:58 +0530

    computation UC-3

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -5,4 +5,4 @@ read b
 read c
 num1=$(echo $a $b $c | awk '{ print $1 + $2 * $3 }' )
 num2=$(echo $a $b $c | awk '{ print $1 * $2 + $3 }' )
-# echo "$num1 and $num2"
+echo "computation one two and three are :" $num1 $num2 $num3
